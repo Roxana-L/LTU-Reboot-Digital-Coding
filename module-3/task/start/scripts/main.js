c5ea05b0dfc0a293eba912d46eee51d20883922d
Roxana <roxana.lupei311@gmail.com>
2021-10-05 20:57:27 +0100

    Update main.js
    Add practice comments, variables, console.logs, document.write

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,34 @@
+/*
+1.  Find out what day it is
+2. If it's Monday, Wednesday or Friday = Exercise
+3. Else = Rest Day
+*/
+
+//This is a simple comment
+
+/*
+This is a multiple 
+line
+comment 
+*/
+
+/*
+let age = 37;
+let name = "Luke";
+
+let message = "It's Tuesday!";
+ */
+
+const productName = "Baked Goods";
+let price = 1.3;
+let quantity = 4;
+let inStock = true;
+let discountAmount = 10;
+
+console.log("Product Name:", productName);
+console.log("Price:", price);
+console.log("Quantity:", quantity);
+console.log("In Stock:", inStock);
+console.log("Discount:", discountAmount);
+
+document.write("<p>" + productName + "</p>");
