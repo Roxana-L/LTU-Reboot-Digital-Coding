3dda94450073e369210fb31c08713a92616f0e70
Roxana <roxana.lupei311@gmail.com>
2021-10-14 20:58:55 +0100

    Update main.js
    Completed tasks 4-8

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -25,10 +25,104 @@ let quantity = 4;
 let inStock = true;
 let discountAmount = 10;
 
-console.log("Product Name:", productName);
-console.log("Price:", price);
-console.log("Quantity:", quantity);
-console.log("In Stock:", inStock);
-console.log("Discount:", discountAmount);
+//console.log("Product Name:", productName);
+//console.log("Price:", price);
+//console.log("Quantity:", quantity);
+//console.log("In Stock:", inStock);
+//console.log("Discount:", discountAmount);
 
-document.write("<p>" + productName + "</p>");
+//document.write("<p>" + productName + "</p>");
+
+//function greeting() {
+//console.log("Hello");
+//}
+//greeting();
+
+//function greeting(name, age) {
+//console.log(`Hello ${name}, you are ${age} years old`);
+//}
+//greeting("Sue", 27);
+
+//function addOne(num) {
+//let newNumber = num + 1;
+//console.log(newNumber);
+//}
+//addOne(5);
+
+//function addOne(num) {return num + 1;
+//}
+//let amount = addOne(5);
+//console.log(amount);
+
+function showProductName() {
+  console.log(productName);
+}
+showProductName();
+
+function totalPrice(productPrice, productQuantity) {
+  console.log(productPrice * productQuantity);
+}
+//totalPrice(price, quantity);
+
+//function checkAvailability(productInStock) {
+////  console.log(productInStock);
+//}
+//checkAvailability(inStock);
+
+const checkAvailability = (productInStock) => console.log(productInStock);
+checkAvailability(inStock);
+
+let global = "Global scope";
+const localScope = () => {
+  let scope = "Scope";
+  return scope;
+};
+const scope = localScope();
+localScope();
+
+console.log(scope);
+console.log(global);
+
+const productDiscount = () => {
+  if (quantity > 1) {
+    let newPrice = discountAmount - price * quantity;
+    console.log(newPrice);
+  } else {
+    console.log(price * quantity);
+  }
+};
+//productDiscount();
+const level = 4;
+switch (level) {
+  case 1:
+    console.log("You are on level 1");
+    break;
+  case 2:
+    console.log("You are on level 2");
+    break;
+  case 3:
+    console.log("You are on level 3");
+    break;
+  case 4:
+    console.log("You are on level 4");
+    break;
+  default:
+    console.log("No level found");
+}
+function drinkOrder(drink, size) {
+  switch (drink) {
+    case "cola":
+      console.log(`${size} : ${drink}`);
+      break;
+    case "lemonade":
+      console.log(`${size} : ${drink}`);
+      break;
+    case "orangeade":
+      console.log(`${size} : ${drink}`);
+      break;
+    default:
+      console.log(`We don't have your drink in stock`);
+      break;
+  }
+}
+drinkOrder("cola", "small");
